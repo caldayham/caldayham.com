d804f43c9e283d6eb2d19a86283464f725e42ded
caldayham <caldayham@gmail.com>
2023-08-14 19:14:34 -0700

    self checkout button

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -11,6 +11,19 @@ export default function ConnectPage() {
       <h1>Connect with Cal!</h1>
       <div className={styles.socialsContainer}>
         <SocialIcons />
+        <Link
+            href={'https://buy.stripe.com/28o9Dl8jhg5A8Uw145'}
+            className={styles.purchaseButton}
+            target='_blank'
+            rel="noopener noreferrer"
+            style={{marginTop:"5px"}}>
+            Stripe &nbsp;
+            <div style={{ width: '22px' }}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+              </svg>
+            </div>
+          </Link>
       </div>
       <p>To connect with cal (via email, phone, or any other supported message) you must first submit something you find interesting.</p>
       <p>As the number of requests to connect increases, so will the bar for what is interesting enough. I absolutely love talking to new people, but I have the same 24 hours in my day you do and can't spend it all talking to people about things I don't find interesting.</p>
