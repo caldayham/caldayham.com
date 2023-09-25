37bc130e01eeed30ff133bffd1dd75c039d596ff
caldayham <caldayham@gmail.com>
2023-09-24 17:16:28 -0700

    removed school post

--- src/app/thoughts/(posts)/thoughtPosts.ts
+++ src/app/thoughts/(posts)/thoughtPosts.ts
@@ -2,14 +2,6 @@ import { Post } from "@/models/post.model";
 
 export const thoughtPosts: Post[] = [
     {
-        path: 'computational-evolution',
-        title: 'Computational Evolution - Exponential Potential.',
-        desc: 'How computers have evolved, and how they will continue to evolve, and how that will affect us.',
-        img: '/imgs/progress-of-computing.png',
-        date: '22-09-2023',
-        tags: ['thought', 'technology'],
-    },
-    {
         path: 'severity-of-being',
         title: 'You Exist, I Think That is Severely Epic',
         desc: 'I guess it makes sense that something exists and is, because true isn-ness would not really be able to be, that said, quite grateful.',
