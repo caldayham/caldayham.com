c1d76f5d1ea3a3f37adbb9c9fd11cdf2787cc377
caldayham <caldayham@gmail.com>
2023-10-23 11:53:14 -0700

    child care page

---
+++ src/app/(odds-ends)/care/page.tsx
@@ -0,0 +1,9 @@
+export default function ChildCarePage() {
+    return (
+        <>
+            <h1>Interesting projects, exciting adventures, safe and happy children.</h1>
+
+            <p>Hello! My name is Cal, I am a certified EMT, current university student studying biology and physiology, and love looking after children!</p>
+        </>
+    )
+}
